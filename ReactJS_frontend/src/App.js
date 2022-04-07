7eee6a25d9f53f9dd52941220206de919dbf519e
Glenn Tay <glenntay131@gmail.com>
2022-04-07 18:29:00 +0800

    render uss reviews on frontend

--- ReactJS_frontend/src/App.js
+++ ReactJS_frontend/src/App.js
@@ -1,19 +1,24 @@
 import { useState } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Select from 'react-select';
 import { Container, Row, Col } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from "./logo.png";
 
-const actions = [
-	{ label: "DocID", value: 1},
-	{ label: "Comment upvotes", value: 2},
-	{ label: "Comment Title", value: 3},
-	{ label: "Comment content", value: 4},
-	{ label: "Comment date", value: 5},
-	{ label: "Rating given", value: 6},
-	{ label: "Reviewer contribution", value: 7},
-	{ label: "Reviewer name", value: 8},
+const FieldSearchactions = [
+	{ label: "reviews:>=0", value: 1 },
+	{ label: "reviews:>=1", value: 2 },
+	{ label: "reviews:>=2", value: 3 },
+	{ label: "reviews:>=3", value: 4 },
+	{ label: "reviews:>=4", value: 5 },
+	{ label: "reviews:>=5", value: 6 },
+];
+
+const SortSearchactions = [
+	{ label: "No. of upvotes(descending)", value: 1 },
+	{ label: "No. of upvotes(ascending)", value: 2 },
+	{ label: "No. of user contributions(descending)", value: 3 },
+	{ label: "No. of user contributions(ascending)", value: 4 },
 ];
 
 function App() {
@@ -21,27 +26,47 @@ function App() {
 	const [results, setResults] = useState([]);
 	const [searchInfo, setSearchInfo] = useState({});
 
-	const [inputValue, setValue] = useState("");
-	const [selectedValue, setSelectedValue] = useState(null);
+	const [errorValue, setError] = useState('false');
+
+	//const [inputValue, setValue] = useState("");
+	const [selectedFieldValue, setSelectedFieldValue] = useState(null);
+	const [selectedSortValue, setSelectedSortValue] = useState(null);
+
+	const [filterBy, setFilter] = useState("");
+	const [SortBy, setSort] = useState("");
 
 	const handleSearch = async e => {
 		e.preventDefault();
 		if (search === '') return;
 
-		const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&
-		prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${search}`;
+		const endpoint = `http://localhost:8080/documents?query="${search}"&filter=&sort=`;
 
+		if (filterBy==="" && SortBy==="") {
+			const endpoint = `http://localhost:8080/documents?query="${search}"&filter=&sort=`;
+		} else if (filterBy==="" && SortBy!=="") {
+			const endpoint = `http://localhost:8080/documents?query="${search}"&filter=&sort=1`;
+		} else {
+			const endpoint = `http://localhost:8080/documents?query="${search}"&filter=1&sort=`;
+		}
+
+		// https://en.wikipedia.org/w/api.php?action=query&list=search&
+		// prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${search}
+
+		//http://localhost:8080/documents?query=${search}
 		const response = await fetch(endpoint);
 
 		if (!response.ok) {
+			setError('true')
 			throw Error(response.statusText);
+		} else {
+			setError('false')
 		}
 
 		const json = await response.json();
 		console.log(json);
 
-		setResults(json.query.search);
-		setSearchInfo(json.query.searchinfo);
+		setResults(json);
+		setSearchInfo(json);
 	}
 
 	const resetSearch = e => {
@@ -51,57 +76,151 @@ function App() {
 	}
 
 	//handle filter search
-	const handleInputChange = value => {setValue(value)};
-	const handleChange = value => {setSelectedValue(value)};
+
+	const handleFieldChange = value => { 
+		console.log('F VALUE: ', value);
+		setSelectedFieldValue(value) 
+		setFilter(value.label)
+		//console.log('F FILTER BY: ', filterBy);
+		//console.log('F SORT BY: ', SortBy);
+	};
+	console.log('F FILTER BY: ', filterBy);
+	console.log('F SORT BY: ', SortBy);
+
+	const handleSortChange = value => { 
+		console.log('S VALUE: ', value);
+		setSelectedSortValue(value) 
+		setSort(value.label)
+		//console.log('S SORT BY: ', SortBy);
+		//console.log('S FILTER BY: ', filterBy);
+	};
+	console.log('S FILTER BY: ', filterBy);
+	console.log('S SORT BY: ', SortBy);
+	// const handleFieldSearch = async e => {
+	// 	console.log('E is: ', e);
+	// 	//e contains {label: ___, value: __}
+
+	// 	const endpoint = `http://localhost:8080/documents?query="${search}"&filter=4&sort=`;
+
+	// 	const response = await fetch(endpoint);
+
+	// 	if (!response.ok) {
+	// 		throw Error(response.statusText);
+	// 	} 
+
+	// 	const json = await response.json();
+	// 	console.log("FIELD SEARCH RESULT IS: ", json);
+
+	// };
+
+	// const handleSortSearch = async e => {
+	// 	console.log('E is: ', e);
+	// 	//e contains {label: ___, value: __}
+
+	// 	const endpoint = `http://localhost:8080/documents?query="${search}"&filter=4&sort=`;
+
+	// 	const response = await fetch(endpoint);
+
+	// 	if (!response.ok) {
+	// 		throw Error(response.statusText);
+	// 	} 
+
+	// 	const json = await response.json();
+	// 	console.log("SORT SEARCH RESULT IS: ", json);
+
+	// };
 
 	return (
 		<div className="App">
 			<header>
 				<div>
 					<Link to="/">
-						<img className='logo' src={logo} alt="" onClick={e => resetSearch()} 
+						<img className='logo' src={logo} alt="" onClick={e => resetSearch()}
 						/>
 					</Link>
 				</div>
 				{/* <h1>USS Advisor</h1> */}
 				<form className="search-box" onSubmit={handleSearch}>
-					<input 
-						type="search" 
+					<input
+						type="search"
 						placeholder="What are you looking for?"
 						value={search}
 						onChange={e => setSearch(e.target.value)}
 					/>
 				</form>
-				{ (searchInfo.totalhits) ? <p>Search Results: {searchInfo.totalhits}</p> : '' }
-				{ (searchInfo.totalhits) ? 
-					<div style={{ display: 'flex', width: 750 }}>
+				{(Object.keys(searchInfo).length !== 0 && errorValue==='false') ? <p>Search Results: {Object.keys(searchInfo).length}</p> : ''}
+				{(errorValue ==='true') ? <p>No search results found! Please refine your search</p> : '' }
+				{(Object.keys(searchInfo).length !== 0) ?
+					<div style={{ display: 'flex', width: 750 , paddingTop: 15 }}>
 						<Row>
 							<Col>
-								<p>Filter by:</p>
+								<p>Field Search:</p>
 							</Col>
-							<Col style={{width:'250px'}}>
-								<Select options={ actions } value={selectedValue} onInputChange={handleInputChange}
-								onChange={handleChange}/>
+							<Col style={{ width: '250px' }}>
+								<Select options={FieldSearchactions} value={selectedFieldValue}
+									onChange={handleFieldChange}/>
 							</Col>
-							<Col style={{marginTop: 5}}>
-								<button onInputChange={handleInputChange} onClick={handleChange}>Unfilter</button>
+							<Col style={{ marginTop: 5 }}>
+								<button onClick={handleFieldChange}>Undo Field Search</button>
+							</Col>
+						</Row>
+
+						<Col style={{ width: '250px' }}>
+						</Col>
+						<Row>
+							<Col>
+								<p>Sort by:</p>
+							</Col>
+							<Col style={{ width: '250px' }}>
+								<Select options={SortSearchactions} value={selectedSortValue}
+									onChange={handleSortChange}/>
+							</Col>
+							<Col style={{ marginTop: 5 }}>
+								<button onClick={handleSortChange}>Undo Sort</button>
 							</Col>
 						</Row>
 					</div> : ""
-					
+
 				}
 			</header>
 			<div className="results">
 				{results.map((result, i) => {
-					console.log("Input value is: ", inputValue);
-					console.log("selected value: ", selectedValue);
-					console.log("results: ", result);
-					const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+					const url = `https://www.tripadvisor.com.sg/Attraction_Review-g294264-d2439664-Reviews-or11280-Universal_Studios_Singapore-Sentosa_Island.html`;
+					
+					//removing [ and ] for display
+					let titleCommentTemp = result.titleComment.replace("[", "");
+					let titleComment = titleCommentTemp.replace("]", "");
+
+					let commentLikeTemp = result.commentLike.replace("[", "");
+					let commentLike = commentLikeTemp.replace("]", "");
+
+					let ratingTemp = result.rating.replace("[", "");
+					let rating = ratingTemp.replace("]", "");
+
+					let dateTemp = result.date.replace("[", "");
+					let date = dateTemp.replace("]", "");
+
+					let userTemp = result.user.replace("[", "");
+					let user = userTemp.replace("]", "");
+
+					let countryTemp = result.country.replace("[", "");
+					let country = countryTemp.replace("]", "");
+
+					let commentTemp = result.contentComment.replace("[", "");
+					let comment = commentTemp.replace("]", "");
+
 					return (
+						
 						<div className="result" key={i}>
-								<h3>{result.title}</h3>
-								<p dangerouslySetInnerHTML={{__html: result.snippet}} ></p>
-								<a href={url} target="_blank">Read more</a>
+							<h3>"{titleComment}"</h3>
+							<p>{commentLike} Likes</p>
+							<p><b>Rating given:</b> {rating}/5</p>
+							<p><b>Date posted</b>: {date}</p>
+							<p><b>Reviewer</b>: {user}</p>
+							<p><b>Country</b>: {country}</p>
+							<p><b>Review</b>:</p>
+							<i><p dangerouslySetInnerHTML={{ __html: comment.replace(new RegExp(search, "gi"), (match)=> `<mark>${match}</mark>`) }}></p></i>
+							<a href={url} target="noreferrer">Read more</a>
 						</div>
 					)
 				})}
